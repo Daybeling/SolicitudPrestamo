3366d93960cf98e1d78b8648f9c4543177d2bc05
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2019-04-09 00:04:50 -0600

    Creando Spinner

--- app/src/main/java/com/example/solicitudprestamo/MainActivity.java
+++ app/src/main/java/com/example/solicitudprestamo/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.solicitudprestamo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner opciones;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        opciones = (Spinner)findViewById(R.id.spinnerSexo);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Opciones, android.R.layout.simple_spinner_item);
+        opciones.setAdapter(adapter);
     }
 }
