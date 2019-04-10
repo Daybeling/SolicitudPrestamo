844a4ba65d8a71e6f696203613795fc0871d1ef7
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2019-04-09 22:54:35 -0600

    Validar campos

--- app/src/main/java/com/example/solicitudprestamo/MainActivity.java
+++ app/src/main/java/com/example/solicitudprestamo/MainActivity.java
@@ -1,22 +1,62 @@
 package com.example.solicitudprestamo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner opciones;
+    Spinner interes;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         opciones = (Spinner)findViewById(R.id.spinnerSexo);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Opciones, android.R.layout.simple_spinner_item);
         opciones.setAdapter(adapter);
+
+        interes = (Spinner)findViewById(R.id.spinnerInteres);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.Intereses, android.R.layout.simple_spinner_item);
+        interes.setAdapter(adapter1);
+    }
+
+    public void onClick(View v){
+        EditText nombre = findViewById(R.id.editTextNombre);
+        String cNombre = nombre.getEditableText().toString();
+        EditText telefono = findViewById(R.id.editTextTelefono);
+        String cTelefono = telefono.getEditableText().toString();
+        EditText cedula = findViewById(R.id.editTextCedula);
+        String cCedula = cedula.getEditableText().toString();
+        EditText direccion = findViewById(R.id.editTextDireccion);
+        String cDireccion = direccion.getEditableText().toString();
+        if(cNombre.isEmpty()){
+            nombre.setError("campo obligatorio");
+        }
+        else if(cTelefono.isEmpty()){
+            telefono.setError("campo obligatorio");
+        }
+        else if(cCedula.isEmpty()){
+            cedula.setError("campo obligatorio");
+        }
+        else if(cDireccion.isEmpty()){
+            direccion.setError("campo obligatorio");
+        }
+        else{
+            Intent inte = new Intent(getBaseContext(), Main2Activity.class);
+            startActivity(inte);
+        }
+
     }
 }
+
+
+
